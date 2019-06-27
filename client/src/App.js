e9645d5ecd65bd5632068e6432340984244d1f2d
jvmCoder <kimg0130@gmail.com>
2019-06-27 23:33:54 +0900

    test server

--- client/src/App.js
+++ client/src/App.js
@@ -18,38 +18,29 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
-const customers = [{
-    'id':1,
-    'image':'https://placeimg.com/64/64/1',
-    'name':'이태호1',
-    'birthday':'780907',
-    'gender':'몰라',
-    'job':'프로글래머',
-  },
-  {
-    'id':2,
-    'image':'https://placeimg.com/64/64/2',
-    'name':'이태호2',
-    'birthday':'780907',
-    'gender':'몰라',
-    'job':'프로글래머',
-  },
-  {
-    'id':3,
-    'image':'https://placeimg.com/64/64/3',
-    'name':'이태호3',
-    'birthday':'780907',
-    'gender':'몰라',
-    'job':'프로글래머',
-  }
-]
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const { classes } = this.props;
-
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
@@ -65,12 +56,10 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
-                return (
-                  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-                );
-              })
-            }
+              this.state.customers ? this.state.customers.map(c => {
+                return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+              }) : ""
+             }
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send(
+        [{
+            'id':1,
+            'image':'https://placeimg.com/64/64/1',
+            'name':'이태호1',
+            'birthday':'780907',
+            'gender':'몰라',
+            'job':'프로글래머',
+          },
+          {
+            'id':2,
+            'image':'https://placeimg.com/64/64/2',
+            'name':'이태호2',
+            'birthday':'780907',
+            'gender':'몰라',
+            'job':'프로글래머',
+          },
+          {
+            'id':3,
+            'image':'https://placeimg.com/64/64/3',
+            'name':'이태호3',
+            'birthday':'780907',
+            'gender':'몰라',
+            'job':'프로글래머',
+          }
+        ]
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
